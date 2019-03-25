0fa01939f3959207788fb2268215a056d2c51c93
Arturo Filio Villa <arturfil@hotmail.com>
2019-03-24 23:34:24 -0400

    displaying the user's profile picture

--- Instagram-clone/UserProfile/UserProfileController.swift
+++ Instagram-clone/UserProfile/UserProfileController.swift
@@ -23,7 +23,9 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath)
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath) as! UserProfileHeader
+        
+        header.user = self.user
         
         return header
     }
@@ -32,6 +34,7 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         return CGSize(width: view.frame.width, height: 200)
     }
     
+    var user: User?
     fileprivate func fetchUser() {
         guard let uid = Auth.auth().currentUser?.uid else {return}
         
@@ -40,11 +43,22 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
             
             guard let dictionary = snapshot.value as? [String: Any] else {return}
             
-            let username = dictionary["username"] as? String
-            self.navigationItem.title = username
+            self.user = User(dictionary: dictionary)
+            self.navigationItem.title = self.user?.username
+            self.collectionView?.reloadData()
             
         }) {(err) in
             print("Failed to fetch user: ", err)
         }
+    }
+}
+
+struct User {
+    let username: String
+    let profileImageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
     }
 }
